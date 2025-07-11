bf2cfcdcd5474afa5876d2c39cf80afc4d283bb8
raginimanjaiah <ragini.manjaiah@gmail.com>
2025-07-11 21:59:11 +0530

    Update dbConnect.py added mongo db

--- my-react-app/src/database/dbConnect.py
+++ my-react-app/src/database/dbConnect.py
@@ -2,16 +2,20 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from pydantic import BaseModel
+from pymongo import MongoClient
 
+from models.product import domainSpecificResponse
 
 # Replace with your actual DB credentials
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://<username>:<password>@localhost:3306/valuesmart_db"
-
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql:/<user>:<pwd>@localhost:3306/<db>"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
-
 Base = declarative_base()
+
+client = MongoClient("mongodb://localhost:27017")
+db = client["valuesmart_db"]
+collectionFoodProcessingMachinary = db["food_processing_machinary"]
+collectionFoodDomainMachinary=db["food_Domain_categories"]
 
 
 def get_db():
@@ -21,3 +25,4 @@ def get_db():
         yield db
     finally:
         db.close()
+
