04eb1f64b055a76fd347fd77a5e9bb15f8aa6ef8
raginimanjaiah <ragini.manjaiah@gmail.com>
2025-07-11 21:57:00 +0530

    Update product.py created new models

--- my-react-app/src/models/product.py
+++ my-react-app/src/models/product.py
@@ -1,25 +1,58 @@
-from pydantic import BaseModel
-from sqlalchemy import Column, Integer, String,Float
-from database.dbConnect import Base
-from typing import Optional
+# from pydantic import BaseModel
+# from sqlalchemy import Column, Integer, String,Float
+# from database.dbConnect import Base
+# from typing import Optional
 
-class ProductResponse(BaseModel):
-    id: int
-    title: str
-    product_description: str
-    price: float
-    image: str
+# class ProductResponse(BaseModel):
+#     id: int
+#     title: str
+#     product_description: str
+#     price: float
+#     image: str
+#
+#
+#     class Config:
+#         from_attributes  = True
+#
+#
+# class Product(Base):
+#     __tablename__ = "product"
+#
+#     id = Column(Integer, primary_key=True, index=True)
+#     title = Column(String(250))
+#     product_description=Column(String(250))
+#     price=Column(Float)
+#     image = Column(String(100), unique=True)
+#
+#
 
+from pydantic import BaseModel, Field
+from typing import List
+
+class foodProcessingMachinaryResponse(BaseModel):
+    id: str = Field(alias="_id")  # Use alias to map MongoDB _id
+    machine_name: str
+    machine_image_location: str
 
     class Config:
-        from_attributes  = True
+        allow_population_by_field_name = True
+        orm_mode = True
+
+class domainSpecificResponse(BaseModel):
+    id: str = Field(alias="_id")  # Use alias to map MongoDB _id
+    domain: str
+    domain_machinary: List[str]
+
+    class Config:
+        validate_by_name = True
+        from_attributes = True
 
 
-class Product(Base):
-    __tablename__ = "product"
+class   FoodProcessingMachinary_domainSpecificResponse(BaseModel):
+    foodProcessingMachinary: List[foodProcessingMachinaryResponse]
+    domainSpecific: List[domainSpecificResponse]
 
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String(250))
-    product_description=Column(String(250))
-    price=Column(Float)
-    image = Column(String(100), unique=True)
+
+def convert_id(doc):
+    doc["_id"] = str(doc["_id"])
+    return doc
